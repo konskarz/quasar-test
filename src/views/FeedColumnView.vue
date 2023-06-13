0f306d4c86cae085debfa821479876ac0a1c93bf
Konstantin Karzanov <k.karzanov@gmail.com>
2023-06-13 11:02:49 +0200

    add feed-column

--- src/views/FeedColumnView.vue
+++ src/views/FeedColumnView.vue
@@ -1,1 +1,48 @@
-<template><q-page class="fit flex flex-center"><h6>Feed column</h6></q-page></template>
+<script setup>
+import { useFetch } from '../store/fetch.js'
+
+const { data } = useFetch()
+function pageStyle(offset, height) {
+  return { height: height - offset + 'px' }
+}
+</script>
+
+<template>
+  <q-page :style-fn="pageStyle">
+    <div class="column no-wrap fit">
+      <div class="col-auto">
+        <q-toolbar class="bg-accent text-white q-pl-lg">
+          <q-toolbar-title>Feed column</q-toolbar-title>
+        </q-toolbar>
+      </div>
+      <div v-if="data" class="col q-pt-md">
+        <div class="row no-wrap full-height overflow-auto">
+          <div class="col-xs-12 col-sm-6 col-lg-4 col-xl-2 column">
+            <q-intersection
+              v-for="item in data"
+              :key="item.id"
+              class="full-width intersection-item"
+              once
+            >
+              <q-item clickable class="q-px-lg">
+                <q-item-section side>
+                  <q-item-label>{{ item.id }}</q-item-label>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>{{ item.name }}</q-item-label>
+                  <q-item-label caption>{{ item.email }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-intersection>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+.intersection-item {
+  min-height: 51.19px;
+}
+</style>
